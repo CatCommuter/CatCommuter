61f322438ba9694c2300c3d8b220b78a3db481ac
RoboMWM <robomwm@users.noreply.github.com>
2018-04-13 12:51:54 -0700

    Hide back button when on main page

--- CatCommuter/MainPage.xaml.cs
+++ CatCommuter/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,6 +26,9 @@ namespace CatCommuter
         public MainPage()
         {
             this.InitializeComponent();
+
+            //hide back button
+            SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility = AppViewBackButtonVisibility.Collapsed;
         }
 
         int times = 0;
