1b6ab6c603ae1c01ddc166ddcbcc06bdeaa76a15
RoboMWM <robomwm@users.noreply.github.com>
2018-04-14 10:59:51 -0700

    use PasswordBox, added classes

---
+++ CatCommuter/Route.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatCommuter
+{
+    class Route
+    {
+        BusStop from;
+        BusStop to;
+        BusLine busLine;
+
+        public Route(BusStop from, BusStop to, BusLine busLine)
+        {
+            this.from = from;
+            this.to = to;
+            this.busLine = busLine;
+        }
+        BusStop getFrom()
+        {
+            return from;
+        }
+
+        BusStop getTo()
+        {
+            return to;
+        }
+
+        BusLine getBusLine()
+        {
+            return busLine;
+        }
+    }
+}
