9bafbeb6d5ef3b57abf7f74c2338060f7dedc584
RoboMWM <robomwm@users.noreply.github.com>
2018-04-14 16:19:05 -0700

    A wild BusStopManager appears

---
+++ CatCommuter/BusStopManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatCommuter
+{
+    class BusStopManager
+    {
+        ISet<BusStop> busStops = new HashSet<BusStop>();
+
+        public BusStopManager()
+        {
+            //Load from storage
+
+            //Create BusLine objects
+
+            //Create BusStop objects
+                //Store BusLine objects in stops as needed.
+        }
+
+        public BusStop getBusStop(string location)
+        {
+            //TODO: return location
+            return null;
+        }
+
+        /**
+         * Returns a list of bus stops near the location, ordered by nearest distance first.
+         */
+        public IList<BusStop> getBusStops(string location, int maxDistance)
+        {
+            return null;
+        }
+    }
+}
