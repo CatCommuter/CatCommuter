999cc0eafe27853365646ed04aef724f74779b31
Justin Lok <32407493+justinlok@users.noreply.github.com>
2018-04-14 17:20:05 -0700

    switch

--- CatCommuter/Preferences.xaml.cs
+++ CatCommuter/Preferences.xaml.cs
@@ -26,5 +26,25 @@ namespace CatCommuter
         {
             this.InitializeComponent();
         }
+
+        private void ToggleSwitch_Toggled(object sender, RoutedEventArgs e)
+        {
+            ToggleSwitch toggleSwitch = sender as ToggleSwitch;
+            if (toggleSwitch != null)
+            {
+                if (toggleSwitch.IsOn == true)
+                {
+                    progress1.IsActive = true;
+                    progress1.Visibility = Visibility.Visible;
+                }
+                else
+                {
+                    progress1.IsActive = false;
+                    progress1.Visibility = Visibility.Collapsed;
+                }
+            }
+        }
+
+
     }
 }
